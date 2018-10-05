ffc7072965b88610ff7e920b719dc19636142caf
Floris Devreese <florisdevreese@hotmail.com>
2018-10-05 16:30:32 +0200

    Add inital project files

---
+++ YOLOv3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace YOLOv3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("\nEnd of program -> Press any key to close the program");
+            Console.ReadKey();
+        }
+    }
+}
