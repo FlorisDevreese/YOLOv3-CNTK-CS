10c441d1e4d91d7dbb1f518a77c4d5983b300886
Floris Devreese <florisdevreese@hotmail.com>
2018-10-09 11:07:36 +0200

    add cntk.gpu dependency, and small test code for dependency. Test if all necessary files are commited to github

---
+++ YOLOv3/Darknet.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace YOLOv3
+{
+    public class Darknet
+    {
+        /// <summary>
+        /// Converts a config file into a list of Dictionaries (blocks) which describe the network to be built.
+        /// </summary>
+        public static List<Dictionary<string, string>> ParseConfigFile(string configFilePath)
+        {
+            var allBlocks = new List<Dictionary<string, string>>();
+            var currentBlock = new Dictionary<string, string>();
+
+            using (StreamReader configFile = File.OpenText(configFilePath))
+            {
+                string line;
+                while((line = configFile.ReadLine()) != null)
+                {
+                    line = line.Trim();
+
+                    // check if this line is the start of a new block
+                    Match newBlockRegex = Regex.Match(line, @"^\[(.+)\]");
+                    if (newBlockRegex.Success)
+                    {
+                        // create new block, and add it to the allBlocks list
+                        currentBlock = new Dictionary<string, string>
+                        {
+                            ["type"] = newBlockRegex.Groups[1].Value
+                        };
+                        allBlocks.Add(currentBlock);
+                    }
+
+                    // check if this line is just a key value pair, and not an empty line or comment line
+                    Match keyValueRegex = Regex.Match(line, @"^([^#].+)=(.+)");
+                    if (keyValueRegex.Success)
+                    {
+                        // add key value pair to the current block
+                        string key = keyValueRegex.Groups[1].Value.Trim();
+                        string value = keyValueRegex.Groups[2].Value.Trim();
+                        currentBlock[key] = value;
+                    }
+                }
+            }
+            
+            return allBlocks;
+        }
+    }
+}
--- YOLOv3/Program.cs
+++ YOLOv3/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CNTK;
+using System;
+using System.IO;
 
 namespace YOLOv3
 {
@@ -6,7 +8,20 @@ namespace YOLOv3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var device = DeviceDescriptor.GPUDevice(0);
+            // Understand the inner workings of YOLO v3 by reading this: https://towardsdatascience.com/yolo-v3-object-detection-53fb7d3bfe6b
+
+            // get the building blocks
+            string configFilePath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, "config/yolov3.cfg");
+            var blocks = Darknet.ParseConfigFile(configFilePath);
+
+            // use the blocks to construct cntk modules for the blocks present in the config file
+
+            // Testing if the CNTK dependecy works
+            Variable features = new Variable();
+            double convWScale = 0.26;
+            var convParams = new Parameter(new int[] { 3, 3, 3, 64 }, DataType.Float, CNTKLib.GlorotUniformInitializer(convWScale, -1, 2), device);
+            Function convFunction = CNTKLib.ReLU(CNTKLib.Convolution(convParams, features, new int[] { 1, 1, 3 } /* strides */));
 
             Console.WriteLine("\nEnd of program -> Press any key to close the program");
             Console.ReadKey();
