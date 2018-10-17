c7b5143a1696bfcb2cc2d5d46af9348be51cbef4
Floris Devreese <florisdevreese@hotmail.com>
2018-10-17 18:06:17 +0200

    Add image pivot to run ONNX model on an static image

---
+++ ONNX model test app/ONNX models/Tiny-YOLOv2.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Media;
+using Windows.Storage;
+using Windows.AI.MachineLearning; // ATTENTION minimum Windows 10 version 1809 (10.0; build 17763) needed
+using System.Linq;
+
+namespace ONNX_model_test_app.ONNX_models
+{
+    public sealed class TinyYoloV2ModelInput
+    {
+        public VideoFrame Image { get; set; }
+    }
+
+    public sealed class TinyYoloV2ModelOutput
+    {
+        public IList<float> Grid { get; set; }
+
+        public TinyYoloV2ModelOutput()
+        {
+            Grid = new List<float>();
+        }
+    }
+
+    // todo the functionality below is only availble after the Windows 10 october update
+    public sealed class TinyYoloV2Model
+    {
+        private LearningModel learningModel;
+        public static async Task<TinyYoloV2Model> CreateModel(StorageFile file)
+        {
+            LearningModel model = await LearningModel.LoadFromStorageFileAsync(file);
+
+
+            // todo investigate the input image description and the outputDescription.
+
+            //Get input and output features of the model
+            List<ILearningModelFeatureDescriptor> inputFeatures = model.InputFeatures.ToList();
+            List<ILearningModelFeatureDescriptor> outputFeatures = model.OutputFeatures.ToList();
+
+            // Retrieve the first input feature which is an image
+            var _inputImageDescription = inputFeatures.FirstOrDefault(
+                feature => feature.Kind == LearningModelFeatureKind.Image) as ImageFeatureDescriptor;
+
+            // Retrieve the first output feature which is a tensor
+            var _outputImageDescription = outputFeatures.FirstOrDefault(
+                feature => feature.Kind == LearningModelFeatureKind.Tensor) as TensorFeatureDescriptor;
+
+            return new TinyYoloV2Model() { learningModel = model };
+        }
+        public async Task<TinyYoloV2ModelOutput> EvaluateAsync(TinyYoloV2ModelInput input)
+        {
+            TinyYoloV2ModelOutput output = new TinyYoloV2ModelOutput();
+            // todo decomment the lines below. + Make them work
+            //LearningModelBinding binding = new LearningModelBinding(learningModel);
+            //binding.Bind("image", input.Image);
+            //binding.Bind("grid", output.Grid);
+            //LearningModelEvaluationResult evalResult = await learningModel.EvaluateAsync(binding, string.Empty);
+            return output;
+        }
+    }
+}
--- ONNX model test app/ViewModels/MainPageViewModel.cs
+++ ONNX model test app/ViewModels/MainPageViewModel.cs
@@ -15,11 +15,18 @@ using ONNX_model_test_app.Views;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using ONNX_model_test_app.ONNX_models;
+using Windows.Media;
+using Windows.Graphics.Imaging;
 
 namespace ONNX_model_test_app.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        #region Webcam pivot
         public DeviceInformationCollection Devices { get { return _Devices; } set { Set(ref _Devices, value); } }
         private DeviceInformationCollection _Devices;
         public object SelectedDevice { get { return _SelectedDevice; } set { Set(ref _SelectedDevice, value as DeviceInformation); CameraDeviceChanged(); } }
@@ -30,12 +37,22 @@ namespace ONNX_model_test_app.ViewModels
         private ResolutionWrapper _SelectedResolution;
         public bool CameraInitialised { get { return _CameraInitialised; } set { Set(ref _CameraInitialised, value); } }
         private bool _CameraInitialised = false;
+        #endregion
+
+        #region Image pivot
+        public SoftwareBitmapSource OriginalImage { get { return _OriginalImage; } set { Set(ref _OriginalImage, value); } }
+        private SoftwareBitmapSource _OriginalImage;
+        public SoftwareBitmapSource ModelOutputImage { get { return _ModelOutputImage; } set { Set(ref _ModelOutputImage, value); } }
+        private SoftwareBitmapSource _ModelOutputImage;
+        #endregion
 
         private bool loadingPageDone = false;
         private bool leavingPage = false;
         private bool changingCamera = false;
         private MediaCapture mediaCapture;
+        private TinyYoloV2Model tinyYoloV2Model;
 
+        #region Webcam pivot
         public async void LoadSettings()
         {
             loadingPageDone = false;
@@ -132,6 +149,61 @@ namespace ONNX_model_test_app.ViewModels
                 }
             }
         }
+        #endregion
+
+        #region Image pivot
+        public async void OpenImageFromFile()
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker
+            {
+                ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail,
+                SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary
+            };
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            var file = await picker.PickSingleFileAsync();
+            if (file == null) // The user cancelled the picking operation
+                return;
+
+            SoftwareBitmap bitmap;
+            using (var stream = await file.OpenReadAsync())
+            {
+                // Create the decoder from the stream
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+
+                // Get the SoftwareBitmap representation of the file
+                // convert bitmap to avoid this error: https://stackoverflow.com/questions/36865978/why-i-get-error-like-this-using-setbitmapasync
+                bitmap = await decoder.GetSoftwareBitmapAsync(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+            }
+
+            // set the original image
+            var bitmapSource = new SoftwareBitmapSource();
+            await bitmapSource.SetBitmapAsync(bitmap);
+            OriginalImage = bitmapSource;
+
+            // todo continue work here after Windows October update
+
+            //// create the output from the model
+            //if (tinyYoloV2Model == null)
+            //    await LoadModel();
+
+            //var input = new TinyYoloV2ModelInput() { Image = VideoFrame.CreateWithSoftwareBitmap(bitmap) };
+            //var output = await tinyYoloV2Model.EvaluateAsync(input);
+
+            // todo interprete output
+
+            // todo use GPU. see doc: https://docs.microsoft.com/en-us/windows/ai/integrate-model
+        }
+
+        private async Task LoadModel()
+        {
+            // todo check if this has to run on a seperate thread so that the UI threas isn't blocked.
+            var modelFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri($"ms-appx:///Assets/Models/Tiny-YOLOv2.onnx"));
+            tinyYoloV2Model = await TinyYoloV2Model.CreateModel(modelFile);
+        }
+        #endregion
 
         private async Task ShowErrorMessage(string message)
         {
